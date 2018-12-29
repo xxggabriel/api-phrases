2a75c84e79080b2eb2849a81fa61d3e3a079f837
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2018-12-28 23:35:53 -0200

    Backup do banco de dados 2018/12/28

---
+++ app/Backups/backup-2018-12-28_23:35:53.sql
@@ -0,0 +1,139 @@
+-- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
+--
+-- Host: localhost    Database: db_word
+-- ------------------------------------------------------
+-- Server version	5.7.24-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `accesses`
+--
+
+DROP TABLE IF EXISTS `accesses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accesses` (
+  `id_phrase` int(11) NOT NULL,
+  `number_of_hits` int(11) DEFAULT '1',
+  PRIMARY KEY (`id_phrase`),
+  CONSTRAINT `fk_accesses_id_phrase` FOREIGN KEY (`id_phrase`) REFERENCES `phrases` (`id_phrase`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accesses`
+--
+
+LOCK TABLES `accesses` WRITE;
+/*!40000 ALTER TABLE `accesses` DISABLE KEYS */;
+INSERT INTO `accesses` VALUES (2,8),(3,14),(4,10);
+/*!40000 ALTER TABLE `accesses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `phrases`
+--
+
+DROP TABLE IF EXISTS `phrases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `phrases` (
+  `id_phrase` int(11) NOT NULL AUTO_INCREMENT,
+  `phrase` varchar(100) DEFAULT NULL,
+  `name_creator` varchar(60) DEFAULT NULL,
+  `email_creator` varchar(120) DEFAULT NULL,
+  PRIMARY KEY (`id_phrase`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `phrases`
+--
+
+LOCK TABLES `phrases` WRITE;
+/*!40000 ALTER TABLE `phrases` DISABLE KEYS */;
+INSERT INTO `phrases` VALUES (2,'O dia vai ser melhor!',NULL,NULL),(3,'É só mais um dia',NULL,NULL),(4,'Porque o mar é salgado?',NULL,NULL);
+/*!40000 ALTER TABLE `phrases` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'db_word'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `create_phrase` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`gabriel`@`localhost` PROCEDURE `create_phrase`(
+pphrase varchar(100), 
+pname_creator varchar(60), 
+pemail_creator varchar(120)
+)
+BEGIN
+	INSERT INTO phrases (phrase, name_creator, email_creator) VALUES (pphrase, pname_creator, pemail_creator);
+    set @id_phrase = (SELECT LAST_INSERT_ID());
+    INSERT INTO accesses (id_phrase) VALUES (@id_phrase);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `get_phrases` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`gabriel`@`localhost` PROCEDURE `get_phrases`(
+pid_phrase int 
+)
+BEGIN
+	SET SQL_SAFE_UPDATES = 0;
+	IF pid_phrase IS NULL THEN
+		SELECT * FROM phrases;
+        UPDATE accesses SET number_of_hits = (number_of_hits + 1) ;
+    ELSE
+		
+        SELECT * FROM phrases WHERE id_phrase = pid_phrase;
+        SET @number_of_hits = (SELECT number_of_hits FROM accesses WHERE pid_phrase = id_phrase);
+        UPDATE accesses SET number_of_hits = (@number_of_hits + 1) WHERE id_phrase = pid_phrase;
+		
+		
+    END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-12-28 23:35:53
